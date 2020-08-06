f8275220d48b486ae9668284e748bc15029d3a2f
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-06 11:53:50 -0300

    Dinamic Router

---
+++ pages/[vehicle]/[person].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+const Person = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+    return (
+    <h2>{router.query.person}'s {router.query.vehicle}</h2>
+      );
+}
+ 
+export default Person;
--- pages/vehicle/bruno.js
+++
@@ -1,7 +0,0 @@
-const Bruno = () => {
-    return (
-        <h2>Bruno's car</h2>
-      );
-}
- 
-export default Bruno;
