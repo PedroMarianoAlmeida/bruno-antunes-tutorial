e39290d35213da753158f31af82409cc93855b89
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-06 12:24:00 -0300

    Populate Link

--- pages/details.js
+++ pages/details.js
@@ -1,13 +1,25 @@
 import Link from 'next/link'
 
+const people = [
+    { v: 'car', name:'bruno' },
+    { v: 'bike', name:'john' },
+    { v: 'airplain', name:'mike' },
+]
+
 const Details = () => {
     return (
         <div>
-            <Link href='/[vehicle]/[person]' as='/car/bruno'>
-                <a>Navigate to Bruno's Car</a>
-            </Link>
+            <h2>Teste</h2>
+            {people.map( e => (
+                <div>
+                <Link href='/[vehicle]/[person]' as={`${e.v}/${e.name}`}>
+                    <a>Navigate to {e.name}'s {e.v}</a>
+                </Link>
+                </div>
+            ))}
+
         </div>
-      );
+    );
 }
- 
+
 export default Details;
